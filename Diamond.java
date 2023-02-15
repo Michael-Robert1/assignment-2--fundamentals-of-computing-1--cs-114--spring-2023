5308d8f6b086fff07b740f2a9dfacd30cead26b7
Michael-Robert1 <mrobert@hartford.edu>
2023-02-15 16:45:44 -0500

    Update Diamond.java
    Get user input for diamond size, create for loop, fix syntax

--- Diamond.java
+++ Diamond.java
@@ -12,15 +12,23 @@ public class Diamond {
   //  Print a diamond based on dimensions entered by the user
   // ---------------------------------------------------------------------------
   public static void main(String[] args) {
-    //Declare and initialize variables
-    int diamondSize;
-
     //Create new objects
     Scanner input = new Scanner(System.in);
 
+    //Print two blank lines to give space for the program
+    System.out.println("");
+    System.out.println("");
 
+    //Request/retrieve user input for diamond size, assign to final int variable
+    System.out.println("Enter a width/height for the diamond: ");
+    final int DIAMOND_SIZE = input.nextInt();
+
+    //Generate and print diamond
+    for(int i = 1; i <= DIAMOND_SIZE; i++) {
+
+    }
 
     //Prevent resource leak
-    scan.close();
+    input.close();
   }
 }
