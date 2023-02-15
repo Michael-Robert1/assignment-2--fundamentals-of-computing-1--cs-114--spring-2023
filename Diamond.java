e317274908865a9b514b886d94c34219321f7a9f
Michael-Robert1 <mrobert@hartford.edu>
2023-02-15 16:12:17 -0500

    Update Diamond.java
    preliminary formatting, imports, declarations, and objects

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,22 @@
+// *****************************************************************************
+// Diamond.java       Author: Michael Robert
+//
+// Submission for Assignment 2 in CS 114
+// *****************************************************************************
+
+//Import libraries
+import java.util.Scanner;
+
 public class Diamond {
+  // ---------------------------------------------------------------------------
+  //  Print a diamond based on dimensions entered by the user
+  // ---------------------------------------------------------------------------
   public static void main(String[] args) {
+    //Declare and initialize variables
+    int diamondSize;
+
+    //Create new objects
+    Scanner scan = new Scanner(System.in);
+
   }
 }
