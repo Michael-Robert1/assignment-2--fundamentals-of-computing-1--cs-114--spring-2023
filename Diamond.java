cc20bc832c55155415b992b40e9c7f9b90d7ee83
Michael-Robert1 <mrobert@hartford.edu>
2023-02-22 18:25:27 -0500

    Update Diamond.java
    Odd working, even not

--- Diamond.java
+++ Diamond.java
@@ -13,29 +13,57 @@ public class Diamond {
   // ---------------------------------------------------------------------------
   public static void main(String[] args) {
     //Declare and Initialize variables
-    String repeatedCharacter;
+    String repeatedString;
+    String repeatedSpace;
 
     //Create new objects
     Scanner input = new Scanner(System.in);
 
     //Print two blank lines to give space for the program
-    System.out.println("");
-    System.out.println("");
+    System.out.println();
+    System.out.println();
 
-    //Request/retrieve user input for diamond size, assign to final int variable
-    System.out.println("Enter a width/height for the diamond: ");
+    //Request&retrieve user input for diamond size, assign to final int variable
+    System.out.println("Enter a width for your Diamond: ");
     final int DIAMOND_SIZE = input.nextInt();
 
     //Change shape of diamond depending on if DIAMOND_SIZE is even or odd
     if(DIAMOND_SIZE % 2 == 1) {
-      repeatedCharacter = "*";
+      repeatedString = "*";
+      repeatedSpace = " ";
     } else {
-      repeatedCharacter = " *";
+      repeatedString = " *";
+      repeatedSpace = "  ";
     }
 
-    //Generate and print diamond layers
-    for(int i = 1; i <= DIAMOND_SIZE; i++) {
-      System.out.println(repeatedCharacter);
+    //Generate and print diamond layers for top half of diamond
+    int row = 0;
+    for( ; row < (DIAMOND_SIZE-1)/2; row++) {
+
+      for(int j = row; j < (DIAMOND_SIZE)/2; j++) {
+        System.out.print(repeatedSpace);
+      }
+
+      for(int j = 0; j < (row*2)+1; j++) {
+        System.out.print(repeatedString);
+      }
+
+      //Return carriage to print on next line
+      System.out.println();
+    }
+
+    //Generate and print diamond layers for bottom half of diamond
+    for( ; row >= 0; row--) {
+
+      for(int j = row; j < (DIAMOND_SIZE)/2; j++) {
+        System.out.print(repeatedSpace);
+      }
+
+      for(int j = 0; j < (row*2)+1; j++) {
+        System.out.print(repeatedString);
+      }
+      //Return carriage to print on next line
+      System.out.println();
     }
 
     //Prevent resource leak
