de4815c952763c91b7e01a8bafc0966dc6481111
Michael-Robert1 <mrobert@hartford.edu>
2023-02-24 17:30:36 -0500

    Update Diamond.java
    long readable "unclever" version, works

--- Diamond.java
+++ Diamond.java
@@ -12,10 +12,6 @@ public class Diamond {
   //  Print a diamond based on dimensions entered by the user
   // ---------------------------------------------------------------------------
   public static void main(String[] args) {
-    //Declare and Initialize variables
-    String repeatedString;
-    String repeatedSpace;
-
     //Create new objects
     Scanner input = new Scanner(System.in);
 
@@ -27,43 +23,83 @@ public class Diamond {
     System.out.println("Enter a width for your Diamond: ");
     final int DIAMOND_SIZE = input.nextInt();
 
+
     //Change shape of diamond depending on if DIAMOND_SIZE is even or odd
     if(DIAMOND_SIZE % 2 == 1) {
-      repeatedString = "*";
-      repeatedSpace = " ";
+      //Run this code if the width for the diamond is odd
+
+      //Generate and print diamond layers for top half of diamond
+      int row = 0;
+      for( ; row < (DIAMOND_SIZE-1)/2; row++) {
+
+        //Print the spaces
+        for(int i = row; i < (DIAMOND_SIZE-1)/2; i++) {
+          System.out.print(" ");
+        }
+
+        //Print the stars
+        for(int i = 0; i < (row*2)+1; i++) {
+          System.out.print("*");
+        }
+
+        //Return carriage to print on next line
+        System.out.println();
+      }
+
+      //Generate and print diamond layers for bottom half of diamond
+      for( ; row >= 0; row--) {
+
+        //Print the spaces
+        for(int i = row; i < (DIAMOND_SIZE-1)/2; i++) {
+          System.out.print(" ");
+        }
+
+        //Print the stars
+        for(int i = 0; i < (row*2)+1; i++) {
+          System.out.print("*");
+        }
+
+        //Return carriage to print on next line
+        System.out.println();
+      }
+
     } else {
-      repeatedString = " *";
-      repeatedSpace = "  ";
-    }
+      //Run this code if the width for the diamond is even
 
-    //Generate and print diamond layers for top half of diamond
-    int row = 0;
-    for( ; row < (DIAMOND_SIZE-1)/2; row++) {
+      //Generate and print diamond layers for top half of diamond
+      int row = 0;
+      for( ; row < (DIAMOND_SIZE-1); row++) {
 
-      for(int j = row; j < (DIAMOND_SIZE)/2; j++) {
-        System.out.print(repeatedSpace);
+        //Print the spaces
+        for(int j = row; j < (DIAMOND_SIZE-1); j++) {
+          System.out.print(" ");
+        }
+
+        //Print the stars
+        for(int j = 0; j < (row+1); j++) {
+          System.out.print("* ");
+        }
+
+        //Return carriage to print on next line
+        System.out.println();
       }
 
-      for(int j = 0; j < (row*2)+1; j++) {
-        System.out.print(repeatedString);
+      //Generate and print diamond layers for middle and bottom half of diamond
+      for( ; row >= 0; row--) {
+
+        //Print the spaces
+        for(int j = row; j < (DIAMOND_SIZE-1); j++) {
+          System.out.print(" ");
+        }
+
+        //Print the stars
+        for(int j = 0; j < (row+1); j++) {
+          System.out.print("* ");
+        }
+
+        //Return carriage to print on next line
+        System.out.println();
       }
-
-      //Return carriage to print on next line
-      System.out.println();
-    }
-
-    //Generate and print diamond layers for bottom half of diamond
-    for( ; row >= 0; row--) {
-
-      for(int j = row; j < (DIAMOND_SIZE)/2; j++) {
-        System.out.print(repeatedSpace);
-      }
-
-      for(int j = 0; j < (row*2)+1; j++) {
-        System.out.print(repeatedString);
-      }
-      //Return carriage to print on next line
-      System.out.println();
     }
 
     //Prevent resource leak
